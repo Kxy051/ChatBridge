be30c9e7cea8c18268a5cb9dfc350a75f20575c4
Kxy051 <a2476675847@gmail.com>
2024-02-18 23:32:25 +0800

    恢复

--- chatbridge/impl/koishi/entry.py
+++ chatbridge/impl/koishi/entry.py
@@ -1,8 +1,5 @@
 import html
 import json
-import sys
-import threading
-import time
 from typing import Optional
 
 import websocket
@@ -20,44 +17,21 @@ chatClient: Optional['KoishiChatBridgeClient'] = None
 
 class KoiBot:
     def __init__(self, config: KoishiConfig):
-        self.usertype_thread = None
-        self.websocket_thread = None
         self.config = config
         self.logger = ChatBridgeLogger('Bot', file_handler=chatClient.logger.file_handler)
         self.current_retry = 0
-        self.websocket_ready = False  # To indicate WebSocket is ready
         websocket.enableTrace(True)
-        self.logger.info(f'Connecting to ws://{self.config.ws_address}:{self.config.ws_port}')
-
-    def start(self):
-        self.websocket_thread = threading.Thread(target=self._start_websocket)
-        self.websocket_thread.start()
-        self.usertype_thread = threading.Thread(target=self.user_type)
-        self.usertype_thread.daemon = True
-        self.usertype_thread.start()
-
-    def _start_websocket(self):
         self.ws = websocket.WebSocketApp(
             f'ws://{self.config.ws_address}:{self.config.ws_port}',
-            on_open=self.on_open,
             on_message=self.on_message,
             on_close=self.on_close
         )
+        self.logger.info(f'Connecting to ws://{self.config.ws_address}:{self.config.ws_port}')
+
+    def start(self):
         if self.config.access_token:
             self.ws.url += f'?access_token={self.config.access_token}'
         self.ws.run_forever()
-
-    def user_type(self):
-        while self.websocket_ready != 'fail':
-            user_input = input().strip().lower()  # Ensure lowercase for comparison
-            if user_input == "stop":
-                self.stop_process()
-
-    def stop_process(self):
-        if hasattr(self, 'ws') and self.ws:
-            self.current_retry = 233
-            self.ws.close()
-        sys.exit(0)
 
     def on_message(self, _, message: str):
         try:
@@ -71,29 +45,8 @@ class KoiBot:
         except Exception as e:
             self.logger.exception(f'Error in on_message(): {e}')
 
-    def on_open(self, *args):
-        self.current_retry = 0
-        self.websocket_ready = 'ready'  # The WebSocket is ready
-        self.logger.info("Type 'stop' to exit.")
-
     def on_close(self, *args):
         self.logger.info("Close connection")
-        while self.current_retry < 6:
-            try:
-                self.logger.info("Retrying in 5 seconds...")
-                time.sleep(5)
-                self.current_retry += 1
-                self._start_websocket()
-            except Exception as e:
-                self.logger.error(f"Connection failed: {e}")
-                self.ws.close()
-        else:
-            if self.current_retry == 6:
-                self.logger.info(f"Maximum retries (6) reached. Exiting...")
-                self.websocket_ready = 'fail'
-                self.stop_process()
-            if self.current_retry > 6:
-                self.websocket_ready = 'fail'
 
     def send_text(self, text):
         if hasattr(self, 'ws') and self.ws:
@@ -121,17 +74,11 @@ def main():
     global chatClient, koishi_bot
     config = utils.load_config(ConfigFile, KoishiConfig)
     chatClient = KoishiChatBridgeClient.create(config)
+    utils.register_exit_on_termination()
     koishi_bot = KoiBot(config)
     koishi_bot.logger.info('Starting KoiBot...')
     koishi_bot.start()
-    try:
-        while True:
-            if koishi_bot.websocket_ready == 'fail':
-                koishi_bot.logger.info('Bye~')
-                koishi_bot.stop_process()
-    except KeyboardInterrupt:
-        koishi_bot.logger.info('Bye~')
-        koishi_bot.stop_process()
+    koishi_bot.logger.info('Bye~')
 
 
 if __name__ == '__main__':
